0c46b5f0bafa1277ff3f4442f17d68d788d80c3a
Hussein <husseinsyed73@gmail.com>
2019-05-29 16:40:56 -0500

    Initial Commit

---
+++ Schooled/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Schooled
+//
+//  Created by Hussein  Syed on 5/29/19.
+//  Copyright © 2019 Hussein  Syed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

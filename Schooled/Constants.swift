cb5b230658b9d6942ec8ed16d206314cf2a477d6
Hussein <husseinsyed73@gmail.com>
2019-06-02 20:33:52 -0500

    Most bug fixes for the user authentication

---
+++ Schooled/Constants.swift
@@ -0,0 +1,25 @@
+//
+// Copyright 2014-2018 Amazon.com,
+// Inc. or its affiliates. All Rights Reserved.
+//
+// Licensed under the Amazon Software License (the "License").
+// You may not use this file except in compliance with the
+// License. A copy of the License is located at
+//
+//     http://aws.amazon.com/asl/
+//
+// or in the "license" file accompanying this file. This file is
+// distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, express or implied. See the License
+// for the specific language governing permissions and
+// limitations under the License.
+//
+
+import Foundation
+import AWSCognitoIdentityProvider
+
+let CognitoIdentityUserPoolRegion: AWSRegionType = .USEast1
+let CognitoIdentityUserPoolId = "us-east-1_uxva8LSxg"
+let CognitoIdentityUserPoolAppClientId = "ggqrih48jtgmog4l1d9opmg1o"
+let CognitoIdentityUserPoolAppClientSecret = "16uq2n9hmakuepmmd7743ruhclfrd140kchc9g1ab3d5gbeig78u"
+let AWSCognitoUserPoolsSignInProviderKey = "UserPool"

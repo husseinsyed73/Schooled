5120439ef91905a3dfd97a2cde43f10e2b07ee7f
omar dadabhoy <omar.dadabhoy@gmail.com>
2019-06-07 22:03:34 -0500

    Created SendViewController

---
+++ Schooled/SendViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SendViewController.swift
+//  Schooled
+//
+//  Created by Omar Dadabhoy on 6/7/19.
+//  Copyright © 2019 Hussein  Syed. All rights reserved.
+//
+
+import UIKit
+
+class SendViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

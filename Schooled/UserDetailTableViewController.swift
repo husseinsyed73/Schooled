04cd3322737d0457dccac94f155e6f334a9f111a
omar dadabhoy <omar.dadabhoy@gmail.com>
2019-06-21 17:15:52 -0500

    menu works but button does not

--- Schooled/UserDetailTableViewController.swift
+++ Schooled/UserDetailTableViewController.swift
@@ -28,7 +28,7 @@ class UserDetailTableViewController : UIViewController {
         // creating the refresh control object
         if self.revealViewController() != nil {
             menuButton.target = self.revealViewController()
-            menuButton.action = "revealToggle:"
+            menuButton.action = #selector(SWRevealViewController.revealToggle(_:))
             self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
         }
         
